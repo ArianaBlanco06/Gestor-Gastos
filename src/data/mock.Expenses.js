01d9c6c21dc668110c4d208c5bd8ec90a631e7fd
ArianaBlanco06 <20234118@aloe.ulima.edu.pe>
2026-05-23 20:48:48 -0500

    Actualizado, reportes, filtros y vista base

---
+++ src/data/mock.Expenses.js
@@ -0,0 +1,7 @@
+// src/data/mock.Expenses.js
+export const mockExpenses = [
+  { id: 1, descripcion: "Almuerzo", categoria: "Alimentación", monto: 25.5, fecha: "2026-05-20" },
+  { id: 2, descripcion: "Transporte", categoria: "Transporte", monto: 8, fecha: "2026-05-21" },
+  { id: 3, descripcion: "Internet", categoria: "Servicios", monto: 120, fecha: "2026-05-22" },
+  { id: 4, descripcion: "Útiles", categoria: "Educación", monto: 45, fecha: "2026-05-23" }
+];
